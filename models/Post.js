ddb0c8c5e52f2ce281d04b9f5813330a9efd3feb
CodeSandbox <support@codesandbox.io>
2023-09-19 10:43:35 +0000

    Sandbox snapshot commit

--- models/Post.js
+++ models/Post.js
@@ -3,15 +3,27 @@ const { Schema, model } = mongoose;
 
 const PostSchema = new Schema(
   {
-    title: String,
-    summary: String,
-    content: String,
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      minlength: [5, "Title should have at least 3 characters"],
+      maxlength: [50, "Title should not exceed 50 characters"],
+    },
+    summary: {
+      type: String,
+      maxlength: [150, "Summary should not exceed 150 characters"],
+    },
+    content: {
+      type: String,
+      required: [true, "Content is required"],
+    },
   },
   {
     timestamps: true,
-  }
+  },
 );
 
 const PostModel = model("Post", PostSchema);
 
 module.exports = PostModel;
+
